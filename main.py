f7f5456b22f3dcdc9eeff9fe6ab62361128a47a5
eduardo-meik <eduardo@meiklabs.com>
2023-10-05 13:47:06 -0300

    first pull

--- main.py
+++ main.py
@@ -1,38 +1,45 @@
 import streamlit as st
-from io import StringIO
-from vector_search import *
 import qa
-from utils import *
+from vector_search import find_match
 
-st.header("Semantic Search Engine for Documents and Q&A")
-url = False
-query = False
-options = st.radio(
-    'Choose task',
-    ('Ask a question','Update the Database'))
+st.header("Asistente para Colegios Particulares Subvencionados en Chile")
 
-if 'Update the Database' in options:
-    url = st.text_input("Enter the url of the document")
-    
-if 'Ask a question' in options:
-    query = st.text_input("Enter your question")
+# Define conversation log
+conversation_log = ""  # Initialize with an empty log or pre-defined logs
 
-button = st.button("Submit")
-  
-if button and (url or query):
-    if 'Update the Database' in options:
-        with st.spinner("Updating Database..."):
-            corpusData = scrape_text_from_url(url)
-            addData(corpusData,url)
-            st.success("Database Updated")
-    if 'Ask a question' in options:
-        with st.spinner("Searching for the answer..."):
-            urls,res = find_match(query,2)
-            context= "\n\n".join(res)
-            st.expander("Context").write(context)
-            prompt = qa.create_prompt(context,query)
-            answer = qa.generate_answer(prompt)
-            st.success("Answer: "+answer)
+# User input for the query
+query = st.text_input("Haz tu Consulta", key="query_input_key")  # Assigning a unique key
+
+if query:  # Check if the user entered a query
+    # Get the matching chunks of text and their similarity scores using the find_match function
+    urls, res, scores = find_match(query, 2)
+
+    # Find the index of the highest score
+    max_score_index = scores.index(max(scores))
+
+    # Use the query refiner
+    refined_query = qa.query_refiner(conversation_log, query)
+    st.write(f"Consulta Refinada: {refined_query}")
+
+    button = st.button("Enviar")
+
+    if button and refined_query:
+        # Displaying title (URL), highest scoring context, and its similarity score
+        context_display = f"Title: {urls[max_score_index]}\n\n{res[max_score_index]}\n\n(Similarity: {scores[max_score_index]*100:.2f}%)"
+        st.expander("Contexto").markdown(context_display)  # Using markdown for structured content
+
+        prompt = qa.create_prompt(res[max_score_index], refined_query)  # Use the highest scoring chunk to create a prompt for the OpenAI model
+        answer = qa.generate_answer(prompt)
+        st.success(f"Respuesta: {answer}")
+
+        # Update conversation log (optional)
+        conversation_log += f"User: {query}\nAssistant: {answer}\n"
+
+
+
+
+
+
 
             
             
---
+++ vector_search.py
@@ -0,0 +1,41 @@
+#vector_search.py
+import streamlit as st
+import pinecone
+import openai
+
+# Access secrets from secrets.toml using st.secrets
+pinecone_api_key = st.secrets["pinecone"]["api_key"]
+pinecone_environment = st.secrets["pinecone"]["environment"]
+index_name = st.secrets["pinecone"]["index_name"]
+
+# Initialize OpenAI client with the API key
+openai.api_key = st.secrets["openai"]["api_key"]
+
+def get_openai_embedding(text):
+    response = openai.Embedding.create(input=text, engine="text-embedding-ada-002")
+    return response["data"][0]["embedding"]
+
+pinecone.init(api_key=pinecone_api_key, environment=pinecone_environment) 
+index = pinecone.Index(index_name)
+
+def addData(corpusData, url):
+    id = index.describe_index_stats()['total_vector_count']
+    for i in range(len(corpusData)):
+        chunk = corpusData[i]
+        chunkInfo = (
+            str(id+i),
+            get_openai_embedding(chunk),  # Use the new embedding function
+            {'title': url, 'context': chunk}
+        )
+        index.upsert(vectors=[chunkInfo])
+
+def find_match(query, k):
+    query_em = get_openai_embedding(query)  # Use the new embedding function
+    result = index.query(query_em, top_k=k, includeMetadata=True)
+    
+    titles = [result['matches'][i]['metadata']['source'] for i in range(k)]
+    contexts = [result['matches'][i]['metadata']['text'] for i in range(k)]
+    scores = [result['matches'][i]['score'] for i in range(k)]  # Collect similarity scores
+    
+    return titles, contexts, scores
+
