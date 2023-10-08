d5f142cc0cd4b2877704bb5fdb16e950525a1747
eduardo-meik <66313196+eduardo-meik@users.noreply.github.com>
2023-10-08 07:27:35 -0300

    Actualizar qa.py

--- qa.py
+++ qa.py
@@ -9,8 +9,8 @@ def generate_answer(prompt):
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
-        temperature=0,
-        max_tokens=256,
+        temperature=0.7,
+        max_tokens=512,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0,
