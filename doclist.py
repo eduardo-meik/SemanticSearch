4598dfe717efb3f38fb8fe4cdbea4a0ea1a29f76
eduardo-meik <66313196+eduardo-meik@users.noreply.github.com>
2023-10-10 16:21:09 -0300

    Create doclist.py

---
+++ doclist.py
@@ -0,0 +1,92 @@
+# doclist.py
+
+DOCLIST = {
+    "Guia-sobre-Normativa-Vigente.pdf": {
+        "link": "https://drive.google.com/file/d/1VwbIjYdVbJMkWTmzVpHLGG2wR-pBXV8E/view?usp=sharing",
+        "title": "Guía sobre normativa vigente para equipos directivos de establecimientos educacionales"
+    },
+    "REGLAMENTO_INTERNO_ORDEN_HIGIENE_SEGURIDAD_2023.txt": {
+        "link": "https://drive.google.com/file/d/1hA6f5h6LRlbywUJodovgxhqNnyibR4yp/view?usp=sharing",
+        "title": "REGLAMENTO  INTERNO  DE  ORDEN,  HIGIENE  Y SEGURIDAD DE CORPORACIÓN EDUCACIONAL WINTERHILL S.A."
+    },
+    "MANUAL_CONVIVENCIA_ESCOLAR_2023.pdf": {
+        "link": "https://drive.google.com/file/d/16qOV-u3EWlrL48Tlb2ayZg-ekHmxY00o/view?usp=sharing",
+        "title": "MANUAL DE LA CONVIVENCIA ESCOLAR"
+    },
+    "Decreto-67_31-DIC-2018.pdf": {
+        "link": "https://drive.google.com/file/d/12nrsXtlwrl89AJMKDsrPkrCV59FALimD/view?usp=sharing",
+        "title": "Decreto 67 - APRUEBA NORMAS MÍNIMAS NACIONALES SOBRE EVALUACIÓN, CALIFICACIÓN Y PROMOCIÓN Y DEROGA LOS DECRETOS EXENTOS N° 511 DE 1997, N° 112 DE 1999 Y N° 83 DE 2001,"
+    },
+    "Decreto-83-EXENTO_05-FEB-2015.pdf": {
+        "link": "https://drive.google.com/file/d/1WHYBJw8X_h9BlS57S6eCfpzX-Y_PzLWG/view?usp=sharing",
+        "title": "Decreto 83 - EXENTO APRUEBA CRITERIOS Y ORIENTACIONES DE ADECUACIÓN CURRICULAR PARA ESTUDIANTES CON NECESIDADES EDUCATIVAS ESPECIALES DE EDUCACIÓN PARVULARIA Y EDUCACIÓN BÁSICA"
+    },    
+    "LEY-20248_01-FEB-2008.pdf": {
+        "link": "https://drive.google.com/file/d/1EmwfwkYHPzRxQSFfSpln_ImV-rG32G5l/view?usp=sharing",
+        "title": "LEY 20248 ESTABLECE LEY DE SUBVENCIÓN ESCOLAR PREFERENCIAL"
+    },
+    "Ley-20418_28-ENE-2010.pdf": {
+        "link": "https://drive.google.com/file/d/1sud-Pt_h5K6XsNJnLllgszoFv-LZoKMX/view?usp=sharing",
+        "title": "LEY 20418 Firma electrónica FIJA NORMAS SOBRE INFORMACIÓN, ORIENTACIÓN Y PRESTACIONES EN MATERIA DE REGULACIÓN DE LA FERTILIDAD"
+    },
+    "Código del Trabajo.txt": {
+        "link": "https://drive.google.com/file/d/1152mayZH-047Zj4Pcq2cVx-pKvZfB9SU/view?usp=sharing",
+        "title": "Código del Trabajo"
+    },
+    "DTO-924_07-ENE-1984.pdf": {
+        "link": "https://drive.google.com/file/d/1k9wzzTXQt5FRwX0SB80Q3PL7bmgE3jxH/view?usp=sharing",
+        "title": "Decreto N° 924 (1984:) reglamenta clases de religión en establecimientos educacionales."
+    },
+     "Decreto-152_09-AGO-2016.pdf": {
+        "link": "https://drive.google.com/file/d/1o38YlxvRuLodYcNYmsCweWUAK3knrhJ2/view?usp=sharing",
+        "title": "Decreto 152 - APRUEBA REGLAMENTO DEL PROCESO DE ADMISIÓN DE LOS Y LAS ESTUDIANTES DE ESTABLECIMIENTOS EDUCACIONALES QUE RECIBEN SUBVENCIÓN A LA EDUCACIÓN GRATUITA O APORTES DEL ESTADO"
+    },
+    "Decreto-170_21-ABR-2010.pdf": {
+        "link": "https://drive.google.com/file/d/1hiYk5LTz7iVbpgsMXtGd8-ME88LnAepP/view?usp=sharing",
+        "title": "Decreto 170 - FIJA NORMAS PARA DETERMINAR LOS ALUMNOS CON NECESIDADES EDUCATIVAS ESPECIALES QUE SERÁN BENEFICIARIOS DE LAS SUBVENCIONES PARA EDUCACIÓN ESPECIAL"
+    },    
+    "ORD_N156_SEGURIDAD_EE.pdf": {
+        "link": "https://drive.google.com/file/d/1q9kINYqbXopLZLeQqTYFOrA__BwPH_en/view?usp=sharing",
+        "title": "ORD. No 0156 - Informa sobre exigencias de seguridad en establecimientos educacionales."
+    },
+    "REGLAMENTO_EVALUACIÓN_PROMOCION_2023.pdf": {
+        "link": "https://drive.google.com/file/d/1-XqFlVFW14F_7tRanA740_TYEucH6g9D/view?usp=sharing",
+        "title": "REGLAMENTO DE EVALUACIÓN Y PROMOCIÓN"
+    },
+    "Ley-20911_02-ABR-2016.pdf": {
+        "link": "https://drive.google.com/file/d/1vsw81c5Nit12INpjA5nKqB5kLxUJ2icE/view?usp=sharing",
+        "title": "Ley 20911 - CREA EL PLAN DE FORMACIÓN CIUDADANA PARA LOS ESTABLECIMIENTOS EDUCACIONALES RECONOCIDOS POR EL ESTADO"
+    },
+    "Ley-21040_24-NOV-2017.pdf": {
+        "link": "https://drive.google.com/file/d/1ks2wivXbqR3mboyFcVkS823ttKA93fXq/view?usp=sharing",
+        "title": "Ley 21040 - CREA EL SISTEMA DE EDUCACIÓN PÚBLICA"
+    },
+    "manualest_2014.txt": {
+        "link": "https://drive.google.com/file/d/1MIjvXVVarT6VD3_M6vb5DCN4_WtC6UTA/view?usp=sharing",
+        "title": "Manual para Establecimientos"
+    },
+    "MONO-501.txt": {
+        "link": "https://drive.google.com/file/d/1_i85Ace1s5G0iUSz0AhNEfzYgkP-E-Oo/view?usp=sharing",
+        "title": "Estándares Indicativos de Desempeño para los Establecimientos Educacionales y sus Sostenedores"
+    },
+    "Ley-20845_08-JUN-2015.pdf": {
+        "link": "https://drive.google.com/file/d/11HQbu1-cs4gFw1T_-a0rjOlygf5DmoSc/view?usp=sharing",
+        "title": "LEY 20845 Firma electrónica DE INCLUSIÓN ESCOLAR QUE REGULA LA ADMISIÓN DE LOS Y LAS ESTUDIANTES, ELIMINA EL FINANCIAMIENTO COMPARTIDO Y PROHÍBE EL LUCRO EN ESTABLECIMIENTOS EDUCACIONALES QUE RECIBEN APORTES DEL ESTADO"
+    },
+    "Ley-20529_27-AGO-2011.pdf": {
+        "link": "https://drive.google.com/file/d/1FLUnae8G5AyOu31fiCdBIq6Dk8JPsuj2/view?usp=sharing",
+        "title": "LEY 20529 - SISTEMA NACIONAL DE ASEGURAMIENTO DE LA CALIDAD DE LA EDUCACIÓN PARVULARIA, BÁSICA Y MEDIA Y SU FISCALIZACIÓN"
+    },
+    "LEY-18956_08-MAR-1990.pdf": {
+        "link": "https://drive.google.com/file/d/1Indn61MpM0OpLJMBnfRuFNQPs8GrbBIt/view?usp=sharing",
+        "title": "Ley 18956 - REESTRUCTURA EL MINISTERIO DE EDUCACION PUBLICA"
+    },
+    "Ley-21109_02-OCT-2018.pdf": {
+        "link": "https://drive.google.com/file/d/11wzVmoPOpWZAo26kqj3BaARrOC0LHXl8/view?usp=sharing",
+        "title": "LEY 21109 Firma electrónica ESTABLECE UN ESTATUTO DE LOS ASISTENTES DE LA EDUCACIÓN PÚBLICA"
+    },
+    "REX 1540.pdf": {
+        "link": "https://drive.google.com/file/d/1ipIDfwQ0GUO9jVwxvb9Xr3OzNUbNa5Wn/view?usp=sharing",
+        "title": "Resolución Exenta No 1540 - Aprueba Organigrama de Ministerio de Educación"
+    }
+}
