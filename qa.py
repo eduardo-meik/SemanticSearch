2964ca1fa9785894520914f440ef0e87dc86696e
eduardo-meik <66313196+eduardo-meik@users.noreply.github.com>
2023-10-12 09:27:18 -0300

    Update qa.py

--- qa.py
+++ qa.py
@@ -2,12 +2,15 @@
 import openai
 
 def create_prompt(context, query):
-    header = "Answer the question as truthfully as possible using the provided context, and if the answer is not contained within the text and requires some latest information to be updated, print 'No tengo suficiente contexto para responder. Prueba consultarlo de otra manera', Answer in spanish all the quesions \n"
+    header = ("Answer the question as truthfully as possible using the provided context, "
+              "and if the answer is not contained within the text and requires some latest information to be updated, "
+              "print 'No tengo suficiente contexto para responder. Prueba consultarlo de otra manera', "
+              "Answer in Spanish all the questions \n")
     return header + context + "\n\n" + query + "\n"
 
 def generate_answer(prompt):
     response = openai.Completion.create(
-        model="text-davinci-003",
+        model="ft:gpt-3.5-turbo-0613:personal::88asXE7W",  # updated model identifier
         prompt=prompt,
         temperature=0.7,
         max_tokens=512,
@@ -20,8 +23,10 @@ def generate_answer(prompt):
 
 def query_refiner(conversation, query):
     response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.All anwser should in Spanish Language \n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+        model="ft:gpt-3.5-turbo-0613:personal::88asXE7W",  # updated model identifier
+        prompt=(f"Using the provided user query and conversation log, craft a relevant question for a knowledge base answer. "
+                "All responses should be in Spanish and pertain only to Chilean subsidized private schools.  \n\n"
+                f"CONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:"),
         temperature=0.1,
         max_tokens=256,
         top_p=1,
@@ -29,3 +34,4 @@ def query_refiner(conversation, query):
         presence_penalty=0
     )
     return response['choices'][0]['text'].strip()
+
